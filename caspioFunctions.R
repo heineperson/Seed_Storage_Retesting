6a5503bb5aa3dd537a32ecdd29763cb6e9eb5633
heineperson <katie.heineman1@gmail.com>
2019-06-04 23:00:03 +0000

    modifing API code for views

--- caspioFunctions.R
+++ caspioFunctions.R
@@ -1,5 +1,7 @@
-# Caspio Functions
-caspio_get_all = function(TableName,login1){
+## Caspio Functions
+
+# Exctracting Table from Caspio API
+caspio_get_table = function(TableName,login1){
   # This table reads the table defined by TableName into R
   # Query parameters specify the row limit (default = 100 & max = 1000)
   url1 = sprintf("https://c4axa460.caspio.com/rest/v1/tables/%s/rows/?q={%s:%s,limit:1000}",TableName,
@@ -24,3 +26,25 @@ caspio_get_all = function(TableName,login1){
   
 }
 
+# Extracting View from Caspio API
+caspio_get_view = function(TableName,login1){
+  # This table reads the table defined by TableName into R
+  # Query parameters specify the row limit (default = 100 & max = 1000)
+  url1 = sprintf("https://c4axa460.caspio.com/rest/v1/views/%s/rows/?q={%s:%s,limit:1000}",TableName,
+                 shQuote("orderby"),shQuote("PK_ID"))
+  print(url1)
+  table_get = GET(url = url1,
+                  add_headers(Authorization=paste("Bearer", content(login1)$access_token),Accept="application/json"))
+
+  # This converts JSON Object to text
+  table_json = content(table_get, as="text")
+  # This converts JSON Object to dataframe
+  table_dataframe = fromJSON(table_json,simplifyDataFrame=TRUE)
+  #This converts dataframe to data.table
+  table_DT = as.data.table(table_dataframe$Result)
+  
+  #while(dim(table_DT)[1]>=1000)
+  return(table_DT)
+  print(dim(table_DT))
+  
+}
